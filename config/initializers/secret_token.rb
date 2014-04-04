870ed69ba925162d98c661e0c09e22cc04c3abd1
prabhakar <undurthi.prabhakar@gmail.com>
2014-04-05 03:18:01 +0530

    Intial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Discuss::Application.config.secret_key_base = 'de97529c1b2be7c6feba8ccfd2dbff48ef216a352bd70b7da3aaea95b7866d54666aa01479203c1bfe2560cce19c358547be056fa4c9d4c2c58dd95c7916270f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Discuss::Application.config.session_store :cookie_store, key: '_discuss_session'
